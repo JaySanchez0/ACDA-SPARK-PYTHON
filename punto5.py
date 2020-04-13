37613c29f80a4bfd3ab9b317d937636f558d3244
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-13 11:26:44 -0500

    punto 5

---
+++ punto5.py
@@ -0,0 +1,18 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+import json
+
+HDFS_MASTER = 'HEAD_NODE_IP'
+
+def jsonMap(line):
+    js = json.loads(line)
+    return (js["lang"],1)
+
+conf = SparkConf()
+conf.setMaster('yarn-client')
+conf.setAppName('spark-wordcount')
+conf.set('spark.executor.instances', 10)
+sc = SparkContext(conf=conf)
+distFile = sc.textFile('hdfs://{0}:9000/tmp/enron/*/*.txt'.format(HDFS_MASTER))
+nonempty_lines = distFile.filter(lambda x: len(x) > 0)
+text = nonempty_lines.map(lambda x: jsonMap).reduceByKey(lambda x,y : x+y)
